b4f617e587e661e3ea70d0c2876c7180e3429865
DragonFire47 <46509577+DragonFire47@users.noreply.github.com>
2024-12-08 01:39:02 -0800

    Add project files.

---
+++ ResetProgress/GUI.cs
@@ -0,0 +1,50 @@
+﻿using CG.Client.Player.Input;
+using UnityEngine;
+using VoidManager.CustomGUI;
+
+namespace ResetProgress
+{
+    internal class GUI : ModSettingsMenu
+    {
+        public override string Name()
+        {
+            return MyPluginInfo.USERS_PLUGIN_NAME;
+        }
+
+        public override void OnOpen()
+        {
+            FirstAsk = false;
+            SecondAsk = false;
+            ThirdAsk = false;
+        }
+
+        bool FirstAsk;
+        bool SecondAsk;
+        bool ThirdAsk;
+
+        public override void Draw()
+        {
+            GUILayout.Label("Reset Profile Menu");
+            GUILayout.Label("There will be no going back.");
+            if (GUILayout.Button("Reset Progress?"))
+            {
+                FirstAsk = true;
+            }
+
+            if (FirstAsk && GUILayout.Button("Are you sure?"))
+            {
+                SecondAsk = true;
+            }
+
+            if (SecondAsk && GUILayout.Button("Are you REALLY sure?!?"))
+            {
+                ThirdAsk = true;
+            }
+
+            if (ThirdAsk && GUILayout.Button("This is the final button. There is no going back."))
+            {
+                DebugInput.CloudProfileReset();
+            }
+        }
+    }
+}
---
+++ ResetProgress/MyPluginInfo.cs
@@ -0,0 +1,17 @@
+#pragma warning disable CS1591
+namespace ResetProgress
+{
+    //Auto-Generated File. Created by PreBuild.ps1
+    public class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = $"{PLUGIN_ORIGINAL_AUTHOR}.{PLUGIN_NAME}";
+        public const string PLUGIN_NAME = "ResetProgress";
+        public const string USERS_PLUGIN_NAME = "Reset Progress";
+        public const string PLUGIN_VERSION = "1.0.0";
+        public const string PLUGIN_DESCRIPTION = "Allows user to reset their player profile.";
+        public const string PLUGIN_ORIGINAL_AUTHOR = "Dragon";
+        public const string PLUGIN_AUTHORS = "Dragon";
+        public const string PLUGIN_THUNDERSTORE_ID = "";
+    }
+}
+#pragma warning restore CS1591
