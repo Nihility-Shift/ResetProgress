3ee51c14d44e5ce6e76c215bcd4d5401c21f83c1
DragonFire47 <46509577+DragonFire47@users.noreply.github.com>
2024-12-30 14:56:00 -0800

    Updated documentation

--- ResetProgress/MyPluginInfo.cs
+++ ResetProgress/MyPluginInfo.cs
@@ -7,10 +7,10 @@ namespace ResetProgress
         public const string PLUGIN_GUID = $"{PLUGIN_ORIGINAL_AUTHOR}.{PLUGIN_NAME}";
         public const string PLUGIN_NAME = "ResetProgress";
         public const string USERS_PLUGIN_NAME = "Reset Progress";
-        public const string PLUGIN_VERSION = "1.0.1";
-        public const string PLUGIN_DESCRIPTION = "Allows user to reset their player profile (levels, cosmetics, acheivements, etc) via the F5 menu.";
+        public const string PLUGIN_VERSION = "1.1.0";
+        public const string PLUGIN_DESCRIPTION = "Allows user to reset their experience or entire player profile (levels, cosmetics, acheivements, etc) via the F5 menu.";
         public const string PLUGIN_ORIGINAL_AUTHOR = "Dragon";
-        public const string PLUGIN_AUTHORS = "Dragon";
+        public const string PLUGIN_AUTHORS = "Dragon, Mest";
         public const string PLUGIN_THUNDERSTORE_ID = "VoidCrewModdingTeam/Reset_Progress";
     }
 }
--- ResetProgress/ReleaseFiles/CHANGELOG.md
+++ ResetProgress/ReleaseFiles/CHANGELOG.md
@@ -1,3 +1,6 @@
+## 1.1.0
+- Added XP Reset option
+
 ## 1.0.1
 - Changed Void Manager requirement to client-side (All-required previously)
 
